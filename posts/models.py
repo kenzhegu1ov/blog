320e0c83fb7f5b6b8aa3b165a40d4be87be163b2
kenzhegu1ov <amantajkenzhegulov@gmail.com>
2023-04-17 19:05:00 +0600

    Initial commit

--- posts/models.py
+++ posts/models.py
@@ -1,9 +1,6 @@
 from django.db import models
 
 
-
-
-
 class Post(models.Model):
     image = models.ImageField(blank=True, null=True)
     title = models.CharField(max_length=255)
@@ -11,3 +8,14 @@ class Post(models.Model):
     rate = models.FloatField()
     created_date = models.DateField(auto_now=True)
     modified_date = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Comment(models.Model):
+    text = models.CharField(max_length=256)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.text
